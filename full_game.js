456e232ecc35da7be6c2e20eda773dbd47474707
noffle <sww@eight45.net>
2018-11-05 12:38:54 +0000

    chat

--- full_game.js
+++ full_game.js
@@ -5,6 +5,7 @@ var neatlog = require('neat-log')
 var neatinput = require('neat-log/input')
 var blit = require('txt-blit')
 var kv = require('kappa-view-kv')
+var list = require('kappa-view-list')
 var ram = require('random-access-memory')
 
 var core = kappa(ram, {valueEncoding:'json'})
@@ -18,6 +19,11 @@ core.use('pos', kv(memdb(), function map (msg, next) {
   }
   next(null, [op])
 }))
+core.use('chat', list(memdb(), function map (msg, next) {
+  if (msg.value.type !== 'chat-message') return next()
+
+  next(null, [msg.value.timestamp])
+}))
 
 core.feed('local', function (err, feed) {
   feed.append({
@@ -28,13 +34,36 @@ core.feed('local', function (err, feed) {
   })
 })
 
-var app = neatlog(view)
+var app = neatlog(view, {
+  fullscreen: true,
+  style: function (start, cursor, end) {
+    if (!cursor) cursor = ' '
+    return start + chalk.underline(cursor) + end
+  }
+})
 app.use(mainloop)
 
 app.input.on('right', moveLocalPlayer.bind(null, 1, 0))
 app.input.on('left', moveLocalPlayer.bind(null, -1, 0))
 app.input.on('up', moveLocalPlayer.bind(null, 0, -1))
 app.input.on('down', moveLocalPlayer.bind(null, 0, 1))
+
+app.input.on('keypress', function (ch, key) {
+  if (!key || !key.name) return
+  if (key.name === 'home') this.neat.input.cursor = 0
+  else if (key.name === 'end') this.neat.input.cursor = this.neat.input.rawLine().length
+  app.render()
+})
+
+app.input.on('enter', function (line) {
+  core.feed('local', function (err, feed) {
+    feed.append({
+      type: 'chat-message',
+      text: line,
+      timestamp: new Date().toISOString()
+    })
+  })
+})
 
 function moveLocalPlayer (xoffset, yoffset) {
   core.feed('local', function (err, feed) {
@@ -60,8 +89,14 @@ function view (state) {
   var chatWindow = drawChatWindowOutline(process.stdout.columns, chatWindowHeight)
   blit(screen, chatWindow, 0, process.stdout.rows - chatWindowHeight - 1)
 
+  // draw chat input
+  blit(screen, drawPrompt(), 1, process.stdout.rows - 1)
+
+  // draw chat messages
+  blit(screen, drawChatMessages(state, process.stdout.columns - 4, chatWindowHeight - 2), 2, process.stdout.rows - chatWindowHeight)
+
   // draw game area
-  var world = drawWorld(state, process.stdout.columns, process.stdout.rows - chatWindowHeight - 1)
+  var world = drawWorld(state, process.stdout.columns, process.stdout.rows - chatWindowHeight - 2)
   blit(screen, world, 0, 0)
 
   return screen.join('\n')
@@ -94,11 +129,29 @@ function drawChatWindowOutline (w, h) {
   return out
 }
 
+function drawChatMessages (state, w, h) {
+  return state.messages.map(function (msg) {
+    return msg.key.substring(0,8) + '> ' + msg.value.text.substring(0, w)
+  })
+}
+
+function drawPrompt () {
+  return [
+    `> ${app.input.line()}`
+  ]
+}
+
 function mainloop (state, bus) {
   state.characters = {}
+  state.messages = []
 
   core.api.pos.onUpdate(function (key, msg) {
     state.characters[key] = { x: msg.value.x, y: msg.value.y }
+    bus.emit('render')
+  })
+
+  core.api.chat.tail(10, function (msgs) {
+    state.messages = msgs
     bus.emit('render')
   })
 
