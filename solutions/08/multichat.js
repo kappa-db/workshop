4cb0f49c10499ee770b27d7567d5e683a20aef1d
noffle <sww@eight45.net>
2018-11-06 13:11:36 +0000

    08

---
+++ solutions/08/multichat.js
@@ -0,0 +1,34 @@
+var discovery = require('discovery-swarm')
+var hypercore = require('hypercore')
+var multifeed = require('multifeed')
+var pump = require('pump')
+
+var multi = multifeed(hypercore, './multichat', {
+  valueEncoding: 'json'
+})
+
+multi.writer('local', function (err, feed) {
+  feed.append({ 
+    type: 'chat-message',
+    nickname: 'cat-lover',
+    text: 'hello world', 
+    timestamp: '2018-11-05T14:26:000Z' // new Date().toISOString()
+  }, function (err, seq) {
+    if (err) throw err
+    console.log('Data was appended as entry #' + seq)
+  })
+
+  process.stdin.on('data', function (data) {
+    feed.append({
+      type: 'chat-message',
+      nickname: 'cat-lover',
+      text: data.toString(),
+      timestamp: new Date().toISOString()
+    })
+  })
+
+  feed.createReadStream({live:true})
+    .on('data', function (data) {
+      console.log(data.timestamp + '> ' + data.text.trim())
+    })
+})
