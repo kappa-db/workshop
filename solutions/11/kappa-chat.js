bf6e53cebcfa19a1b25a4f11d5c59a3bb9e6b115
noffle <sww@eight45.net>
2018-11-06 13:40:42 +0000

    11

---
+++ solutions/11/kappa-chat.js
@@ -0,0 +1,37 @@
+var kappa = require('kappa-core')
+var ram = require('random-access-memory')
+var memdb = require('memdb')
+var list = require('kappa-view-list')
+
+var timestampView = list(memdb(), function (msg, next) {
+    if (msg.value.timestamp && typeof msg.value.timestamp === 'string') {
+        // sort on the 'timestamp' field
+        next(null, [msg.value.timestamp])
+    } else {
+        next()
+    }
+})
+
+var core = kappa('./multichat', {valueEncoding: 'json'})
+core.use('chats', timestampView)
+
+core.ready(function () {
+  core.api.chats.tail(10, function (msgs) {
+    console.log('--------------')
+    msgs.forEach(function (msg) {
+      console.log(msg.value.timestamp + '> ' + msg.value.text.trim())
+    })
+  })
+})
+
+process.stdin.on('data', function (data) {
+  core.feed('local', function (err, feed) {
+    feed.append({
+      type: 'chat-message',
+      nickname: 'cat-lover',
+      text: data.toString(),
+      timestamp: new Date().toISOString()
+    })
+  })
+})
+
