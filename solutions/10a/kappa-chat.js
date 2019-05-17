abda44e2c57ec92c4eef55cb92346cc1ec5fe2ec
noffle <sww@eight45.net>
2019-05-17 22:50:30 +0200

    specify exercise for 10a

--- solutions/10a/kappa-chat.js
+++ solutions/10a/kappa-chat.js
@@ -3,5 +3,6 @@ var kappa = require('kappa-core')
 var core = kappa('./multichat', {valueEncoding: 'json'})
 core.feed('local', function (err, feed) {
   // `feed` is just a hypercore instance, just like the first few exercises.
+  console.log(feed.key.toString('hex'))
 })
 
