07fd20418742ef97ab4ac46534f5f564be9660e0
Saul Blanco Tejero <saul.blanco.tejero@iesjulianmarias.es>
2020-04-07 10:51:43 +0200

    Read messages from data properly formatted

--- solutions/05/single-chat.js
+++ solutions/05/single-chat.js
@@ -3,12 +3,12 @@ var feed = hypercore('./single-chat-feed', {
   valueEncoding: 'json'
 })
 
-feed.append({ 
+feed.append({
   type: 'chat-message',
   nickname: 'cat-lover',
-  text: 'hello world', 
+  text: 'hello world',
   timestamp: '2018-11-05T14:26:000Z' // new Date().toISOString()
-}, function (err, seq) {
+}, function(err, seq) {
   if (err) throw err
   console.log('Data was appended as entry #' + seq)
 })
@@ -17,16 +17,16 @@ feed.append({
 //   console.log('msg', msg)
 // })
 
-process.stdin.on('data', function (data) {
+process.stdin.on('data', function(data) {
   feed.append({
     type: 'chat-message',
     nickname: 'cat-lover',
-    text: data.toString(),
+    text: data.toString().trim(),
     timestamp: new Date().toISOString()
   })
 })
 
-feed.createReadStream({live:true})
-  .on('data', function (data) {
-    console.log(data.timestamp + '> ' + data.text.trim())
+feed.createReadStream({ live: true })
+  .on('data', function(data) {
+    console.log(`<${data.timestamp}> ${data.nickname}: ${data.text}`)
   })
