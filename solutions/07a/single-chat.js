9f075dc3b0876bca42c28c69efcc350a6a88bd73
Saul Blanco Tejero <saul.blanco.tejero@iesjulianmarias.es>
2020-04-07 11:13:01 +0200

    Updating solution 7a, messages properly formatted

--- solutions/07a/single-chat.js
+++ solutions/07a/single-chat.js
@@ -8,7 +8,7 @@ feed.append({
   nickname: 'cat-lover',
   text: 'hello world',
   timestamp: '2018-11-05T14:26:000Z' // new Date().toISOString()
-}, function (err, seq) {
+}, function(err, seq) {
   if (err) throw err
   console.log('Data was appended as entry #' + seq)
 })
@@ -17,21 +17,21 @@ feed.append({
 //   console.log('msg', msg)
 // })
 
-process.stdin.on('data', function (data) {
+process.stdin.on('data', function(data) {
   feed.append({
     type: 'chat-message',
     nickname: 'cat-lover',
-    text: data.toString(),
+    text: data.toString().trim(),
     timestamp: new Date().toISOString()
   })
 })
 
-feed.createReadStream({live:true})
-  .on('data', function (data) {
-    console.log(data.timestamp + '> ' + data.text.trim())
+feed.createReadStream({ live: true })
+  .on('data', function(data) {
+    console.log(`<${data.timestamp}> ${data.nickname}: ${data.text}`)
   })
 
-feed.ready(function () {
+feed.ready(function() {
   console.log('public key:', feed.key.toString('hex'))
   console.log('discovery key:', feed.discoveryKey.toString('hex'))
   console.log('secret key:', feed.secretKey.toString('hex'))
