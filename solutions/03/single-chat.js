093d359a2199137aaa33dd420ab73ec2f691e63f
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-02-04 01:54:54 +0100

    Update exercise 3
    Only keep the relevant code

--- solutions/03/single-chat.js
+++ solutions/03/single-chat.js
@@ -3,16 +3,6 @@ var feed = hypercore('./single-chat-feed', {
   valueEncoding: 'json'
 })
 
-feed.append({
-  type: 'chat-message',
-  nickname: 'cat-lover',
-  text: 'hello world',
-  timestamp: '2018-11-05T14:26:000Z' // new Date().toISOString()
-}, function (err, seq) {
-  if (err) throw err
-  console.log('Data was appended as entry #' + seq)
-})
-
 feed.get(0, function (err, msg) {
   console.log('msg', msg)
 })
