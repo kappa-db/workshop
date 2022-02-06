1f4f5647537bfa369d380063cf9940874e3fbec8
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-02-06 17:57:39 +0100

    Rewrite build script using hightlight.js + upgrade marked

--- build.js
+++ build.js
@@ -1,31 +1,64 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var marked = require('marked')
-var path = require('path')
-var rimraf = require('rimraf')
-
-var css = fs.readFileSync(path.join(__dirname, 'css', 'github-markdown.css'), 'utf-8')
-var base = path.join(__dirname, 'problems')
+const fs = require('fs')
+const marked = require('marked')
+const path = require('path')
+const rimraf = require('rimraf')
+const hljs = require('highlight.js')
+const base = path.join(__dirname, 'problems')
 
 rimraf.sync(path.join(__dirname, 'build'))
-fs.readdirSync(base).forEach(function (name) {
-  var input = path.join(base, name)
-  var output = path.join(__dirname, 'build', name.replace('.md', '.html'))
-  var html = marked(fs.readFileSync(input, 'utf-8'))
-  var file = '<html><head><title>Problem ' +
-    name.replace('.md', '') +
-    '</title><style>\nbody { padding: 40px; }\n' +
-    css +
-    '</style></head><body class="markdown-body">' +
-    html +
-    '</body></html>'
 
-  try {
-    fs.mkdirSync(path.join(__dirname, 'build'))
-  } catch (err) {
-    // do nothing
+marked.setOptions({
+  highlight: function (code, lang) {
+    const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+    return hljs.highlight(code, { language }).value
+  },
+  langPrefix: 'hljs language-'
+})
+
+try {
+  fs.mkdirSync(path.join(__dirname, 'build'))
+} catch (err) {
+  // do nothing
+}
+
+const css = fs.readFileSync('./node_modules/highlight.js/styles/github-dark.css', 'utf8')
+const style = `<style>
+  body {
+    padding: 40px;
   }
+  .markdown-body {
+    -webkit-text-size-adjust: 100%;
+    text-size-adjust: 100%;
+    color: #333;
+    font-family: "Helvetica Neue", Helvetica, "Segoe UI", Arial, freesans, sans-serif;
+    font-size: 16px;
+    line-height: 1.6;
+    word-wrap: break-word;
+    padding: 5rem 2rem 8rem 2rem;
+    width: 60rem;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  ${css}
+</style>`
+
+
+fs.readdirSync(base).forEach(name => {
+  const input = path.join(base, name)
+  const output = path.join(__dirname, 'build', name.replace('.md', '.html'))
+
+  const html = marked.parse(fs.readFileSync(input, 'utf-8'))
+  const file = `<html>
+    <head>
+      <title>Problem ${name.replace('.md', '')}</title>
+      ${style}
+    </head>
+    <body class="markdown-body">
+      ${html}
+    </body>
+  </html>`
 
   fs.writeFileSync(output, file)
 })
